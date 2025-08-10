aa86c87682c18882e0bfa2660a69c2bab29f4652
MarkManukyan <manukanmark90@gmail.com>
2025-08-10 14:15:59 +0300

    Pervoe

---
+++ src/__tests__/sortHeroes.test.js
@@ -0,0 +1,15 @@
+import sortHeroes from '../sortHeroes';
+
+test('sorts heroes by health in descending order', () => {
+    const heroes = [
+        { name: 'мечник', health: 10 },
+        { name: 'маг', health: 100 },
+        { name: 'лучник', health: 80 },
+    ];
+    const sorted = sortHeroes(heroes);
+    expect(sorted).toEqual([
+        { name: 'маг', health: 100 },
+        { name: 'лучник', health: 80 },
+        { name: 'мечник', health: 10 },
+    ]);
+});
